e7969178045fc6f1dca89085dd8b6370370a3114
Maryan Stoykov <stojkovmarjan@gmail.com>
2017-10-08 12:53:05 +0200

    Add project files.

---
+++ MVC7Days_2/Controllers/TestController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC7Days_2.Controllers
+{
+    public class Customer
+    {
+        public string CustomName { get; set; }
+        public string Address { get; set; }
+    }
+
+    public class TestController : Controller
+    {
+        // GET: Test
+        /*
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public string GetString()
+        {
+            return "Hallo world is outdated!";
+        }
+        */
+        // sekoj public metod ako ne e oznacen so [NonAction] moze da se povika
+        public Customer GetCustomer()
+        {
+            Customer c = new Customer();
+            c.CustomName = "Customer 1";
+            c.Address = "Address 1";
+            return c;
+        }
+        [NonAction]//ne moze da se povika, ne e action method
+        public string SimpleMethod()
+        {
+            return "dsfsdf sdf sdf sdfsdfsdf";
+        }
+    }
+}
---
+++ MVC7Days_2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC7Days_2.MvcApplication" Language="C#" %>
